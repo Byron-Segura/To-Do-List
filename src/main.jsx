793107c21ddc49393da369afe685776c475d68b2
Byron SeMo <byronsemo@gmail.com>
2023-05-10 17:18:08 -0700

    Minor adjustments

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { TodosContextProvider } from './context/todos'
 import { ThemeContextProvider } from './context/theme'
 import './index.css'
 
