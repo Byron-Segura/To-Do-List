4633e88b27deb0d583f8004ae6b5ecfc086f9997
Byron SeMo <byronsemo@gmail.com>
2023-05-11 21:44:46 -0700

    Add todos store hook

--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -1,12 +1,31 @@
-import { useContext } from 'react'
-import { TodosContext } from '../context/todos'
+import { useTodosList } from '../store/todos'
 
-export function useTodos () {
-  const todosContext = useContext(TodosContext)
+export function useTodosStore () {
+  const addTodo = useTodosList(state => state.addTodo)
+  const deleteTodos = useTodosList(state => state.deleteTodo)
+  const edit = useTodosList(state => state.editTodo)
+  const complete = useTodosList(state => state.completeTodo)
+  const reset = useTodosList(state => state.resetTodos)
+  const todos = useTodosList(state => state.todos)
 
-  if (todosContext === undefined) {
-    throw new Error('useTheme must be used within a ThemeProvider')
+  const createToDo = (data) => {
+    addTodo(data)
   }
 
-  return todosContext
+  const deleteTodo = (data) => {
+    deleteTodos(data)
+  }
+
+  const resetTodos = () => {
+    reset()
+  }
+
+  const updateTodo = (data) => {
+    edit(data)
+  }
+
+  const completeTodo = (data) => {
+    complete(data)
+  }
+  return { createToDo, completeTodo, deleteTodo, todos, updateTodo, resetTodos }
 }
