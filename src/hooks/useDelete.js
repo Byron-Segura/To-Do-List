8327486fe4eac19486f948af4a77cfd53da24c25
Byron SeMo <byronsemo@gmail.com>
2023-05-16 20:28:18 -0700

    Update delete custom hook

--- src/hooks/useDelete.js
+++ src/hooks/useDelete.js
@@ -1,36 +1,17 @@
-import { useEffect, useState } from 'react'
 import { useTodos } from './useTodos'
 
-const initialDelete = {
-  confirmDelete: false,
-  id: null
-}
-
 export function useDelete () {
-  const { deleteTodo, resetTodos } = useTodos()
-  const [valueToDelete, setValueToDelete] = useState(initialDelete)
-  const { openModal } = useTodos()
+  const { deleteTodo, resetTodos, openModal } = useTodos()
 
   const handleDelete = (data) => {
     if (data) {
-      openModal(true)
-      const newToDelete = {
-        ...valueToDelete,
-        id: data
-      }
-      setValueToDelete(newToDelete)
+      deleteTodo(data)
+      openModal(false)
+    } else {
+      resetTodos()
+      openModal(false)
     }
   }
 
-  useEffect(() => {
-    if (valueToDelete.id && valueToDelete.confirmDelete) {
-      deleteTodo(valueToDelete.id)
-      setValueToDelete(initialDelete)
-    } else if (valueToDelete.confirmDelete) {
-      resetTodos()
-      setValueToDelete(initialDelete)
-    }
-  }, [valueToDelete])
-
-  return { deleteTodo, resetTodos, handleDelete, valueToDelete, setValueToDelete }
+  return { deleteTodo, resetTodos, handleDelete }
 }
